65cbdfc6423d375ea247de6180e2971899615def
Lucas Bicalho <lucassantosbicalho@gmail.com>
2025-09-21 17:42:06 +0100

    Add validation for group_by query verb
    Refactor code by extracting validation logic into separate functions to be reused:
    - has_extra_commas() to detect extra commas
    - has_invalid_field_name() to validate field names

--- R/val.R
+++ R/val.R
@@ -1,4 +1,4 @@
-#' Validate query verbs
+#' Validate query verbs.
 #' 
 #' This internal function checks if the given verbs lie among the supported verbs, and dispatch
 #' the specific validation for each verb.
@@ -37,6 +37,37 @@ validate_verbs <- function(verbs) {
   invisible(TRUE)
 }
 
+#' Check extra commas in list of fields.
+#' 
+#' This internal function checks if a list of fields has extra commas between them, or at begining or at the end of the list.
+#' 
+#' @param value A character with the fields separated by comma.
+#' @return Returns `TRUE` if leading comma is found; returns `FALSE` otherwise.
+#' @keywords internal
+has_extra_commas <- function(value) {
+  if (value == "" || grepl("^,|,$|,,", value)) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+#' Check invalid field names
+#' 
+#' This internal function checks if a list of fields has common invalid names, such as fields beginning with a number.
+#' 
+#' @param value A character with the fields separated by comma.
+#' @return Returns `TRUE` if invalid field name is found; returns `FALSE` otherwise.
+#' @keywords internal
+has_invalid_field_name <- function(value) {
+  parts <- trimws(strsplit(value, ",", fixed = TRUE)[[1]])
+  valid_name <- "^[A-Za-z_][A-Za-z0-9_]*$"
+  bad <- parts[!grepl(valid_name, parts)]
+  if (length(bad)) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
 #' Validate the 'select' clause of an ODSQL query.
 #' 
 #' This internal function validates the `select` clause, ensuring that the specified fields
@@ -52,22 +83,22 @@ validate_select <- function(value) {
   if (identical(value, "*") || identical(value,"")) return(invisible(TRUE))
   
   # Check for extra commas or empty value
-  if (value == "" || grepl("^,|,$|,,", value)) {
+  if (has_extra_commas(value)) {
     cli::cli_abort(c(
       "x" = "Check select clause for extra commas or empty value."
     ))
   }
 
   # Check field names
-  parts <- trimws(strsplit(value, ",", fixed = TRUE)[[1]])
-  valid_name <- "^[A-Za-z_][A-Za-z0-9_]*$"
-  bad <- parts[!grepl(valid_name, parts)]
-  if (length(bad)) {
-    cli::cli_abort(c(
-      "x" = "Invalid field name(s) in select clause: {bad}"
-    ))
+  fields <- trimws(strsplit(value, ",", fixed = TRUE)[[1]])
+  for(field in fields){
+    if (has_invalid_field_name(field)) {
+      cli::cli_abort(c(
+        "x" = "Invalid field name {.val {field}} in select clause."
+      ))
+    }
   }
-
+  
   invisible(TRUE)
   
 }
@@ -139,7 +170,7 @@ validate_where <- function(value) {
 validate_limit <- function(value) {
   if (!is.numeric(value) || length(value) != 1 || is.na(value)) {
     cli::cli_abort(c(
-      "x" = "The limit argument must be a non-missing scalar number."
+      "x" = "The limit argument must be a non-missing integer number."
     ))
   }
 
@@ -151,7 +182,7 @@ validate_limit <- function(value) {
 
   if (value < -1 || value > 100) {
     cli::cli_abort(c(
-      "x" = "The limit argument must be between -1 and 100."
+      "x" = "The limit argument must be an integer between -1 and 100."
     ))
   }
 
@@ -159,13 +190,34 @@ validate_limit <- function(value) {
   
 }
 
+#' Validate the 'group_by' clause of an ODSQL query.
+#' 
+#' This internal function validates the `group_by` clause, ensuring that the specified fields
+#' have valid syntax, with no extra commas or invalid names.
+#' 
+#' @param value A string representing the fields to be aggregated, separated by commas.
+#' 
+#' @return Returns invisible `TRUE` if `group_by` clause is valid; otherwise, raises an error.
+#' 
+#' @keywords internal
 validate_group_by <- function(value) {
-  # TODO:
-  # Documentation of this function,
-  # Use the following validations from select clause:
-  # - check extra commas
-  # - check valid field names
+
+  if (has_extra_commas(value)) {
+    cli::cli_abort(c(
+      "x" = "Check group_by clause for extra commas or empty value."
+    ))
+  }
+
+  fields <- trimws(strsplit(value, ",", fixed = TRUE)[[1]])
+  for(field in fields){
+    if (has_invalid_field_name(field)) {
+      cli::cli_abort(c(
+        "x" = "Invalid field name {.val {field}} in group_by clause."
+      ))
+    }
+  }
 }
+
 
 validate_sort <- function(value) {
   # TODO:
